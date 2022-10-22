816854f7a268fdf16a10e282e3427f034a02023f
YevaKolesova <kolesova.yeva@gmail.com>
2022-10-22 16:52:53 +0300

    .

--- 29_JS_14/assets/js/slick_slider.dev.js
+++ 29_JS_14/assets/js/slick_slider.dev.js
@@ -12,7 +12,32 @@ $(function () {
     dots: true,
     arrows: false
   });
-});
+}); // $(function(){
+//   $('.news_slider_wrapper').slick({
+//       lazyLoad: 'ondemand',
+//       infinite: false,
+//       // autoplay:true,
+//       // autoplaySpeed:3000,
+//       speed: 1400,
+//       slidesToShow: 3,
+//       slidesToScroll: 1,
+//       dots: true,
+//       prevArrow: '<button type="button" class="slick-prev slick-arrow"><span class="icon-uniE901 arrow-icon"></span></button>',
+//       nextArrow: '<button type="button" class="slick-next slick-arrow"><span class="icon-uniE901 arrow-icon"></span></button>',
+//       responsive: [
+//         {
+//         breakpoint: 970,
+//         settings: {
+//           lazyLoad: 'ondemand',
+//           slidesToShow: 2,
+//           slidesToScroll: 1,
+//           infinite: true,
+//         }
+//       }
+//       ]
+//     });
+// });
+
 $(function () {
   $('.news_slider_wrapper').slick({
     lazyLoad: 'ondemand',
@@ -22,8 +47,32 @@ $(function () {
     speed: 1400,
     slidesToShow: 3,
     slidesToScroll: 1,
-    dots: true,
     prevArrow: '<button type="button" class="slick-prev slick-arrow"><span class="icon-uniE901 arrow-icon"></span></button>',
-    nextArrow: '<button type="button" class="slick-next slick-arrow"><span class="icon-uniE901 arrow-icon"></span></button>'
+    nextArrow: '<button type="button" class="slick-next slick-arrow"><span class="icon-uniE901 arrow-icon"></span></button>',
+    responsive: [{
+      breakpoint: 1023,
+      settings: {
+        lazyLoad: 'ondemand',
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    }, {
+      breakpoint: 769,
+      settings: {
+        lazyLoad: 'ondemand',
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 569,
+      settings: {
+        lazyLoad: 'ondemand',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+      }
+    }]
   });
 });
